893dc220a90edc9b304ec8e9a1357eed72e07c0d
Caleb Luster <cwl117@outlook.com>
2022-02-28 15:11:22 -0600

    Added tests for Employee. at first it wasn't working so I added all the tests thinking that was the issue but it turns out you are suppused to run 'npm test' not 'npm run test' -_-

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,24 @@
-const { expect } = require("@jest/globals");
-const { test } = require("picomatch");
 const Employee = require("../lib/Employee.js");
+const employee = new Employee("Caleb", "123", "testing@fakeemail.com");
 
+test("test the constructor function to see if it properly pairs", () => {
+  expect(employee.name).toBe("Caleb");
+  expect(employee.ID).toBe("123");
+  expect(employee.email).toBe("testing@fakeemail.com");
+});
+
+test("tests the getName() function to make sure its properly working", () => {
+  expect(employee.getName()).toBe("Caleb");
+});
+
+test("tests the getId() function to make sure its properly working", () => {
+  expect(employee.getId()).toBe("123");
+});
+
+test("tests the getEmail() function to make sure its properly working", () => {
+  expect(employee.getEmail()).toBe("testing@fakeemail.com");
+});
+
+test("tests the getRole() function to make sure its properly working", () => {
+  expect(employee.getRole()).toBe("Employee");
+});

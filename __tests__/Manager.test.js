ee012e2b30927c583404d56a4eeeb3aada41d053
Caleb Luster <cwl117@outlook.com>
2022-02-28 15:32:03 -0600

    thought I was doing good and started adding next test then realized I am not so sure how to inherit class properitys and methods from parent elements, so after googling and watching a couple youtube videos I found I was doing it correctly as far as testing, and also learned to use the super command. Super cool (lol)

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,28 @@
+const Manager = require("../lib/Manager.js");
+const manager = new Manager("Caleb", "123", "testing@fakeemail.com");
+
+test("test the constructor function to see if it properly pairs", () => {
+  expect(manager.name).toBe("Caleb");
+  expect(manager.ID).toBe("123");
+  expect(manager.email).toBe("testing@fakeemail.com");
+});
+
+// tests to make sure that the return function for the getName() method is working properly
+test("tests the getName() function to make sure its properly working", () => {
+  expect(manager.getName()).toBe("Caleb");
+});
+
+// tests to make sure that the return function for the getId() method is working properly
+test("tests the getId() function to make sure its properly working", () => {
+  expect(manager.getId()).toBe("123");
+});
+
+// tests to make sure that the return function for the getEmail() method is working properly
+test("tests the getEmail() function to make sure its properly working", () => {
+  expect(manager.getEmail()).toBe("testing@fakeemail.com");
+});
+
+// tests to make sure that the return function for the getRole() method is working properly
+test("tests the getRole() function to make sure its properly working", () => {
+  expect(manager.getRole()).toBe("Manager");
+});

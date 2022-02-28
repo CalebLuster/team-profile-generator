27bd2e288f0e5413f5fed9b077f0961c23c8b76e
Caleb Luster <cwl117@outlook.com>
2022-02-28 14:45:29 -0600

    was headed in the wrong direction but with youtube and google was able to get the Employee js constructor written

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,4 @@
+const { expect } = require("@jest/globals");
+const { test } = require("picomatch");
+const Employee = require("../lib/Employee.js");
+
---
+++ index.js
@@ -0,0 +1,26 @@
+// const inquirer = require("inquirer");
+// const fs = require("fs");
+// const path = require("path");
+
+// const questions = [
+//   {
+//     type: "",
+//     name: "",
+//     message: "",
+//   },
+//   {
+//     type: "",
+//     name: "",
+//     message: "",
+//   },
+//   {
+//     type: "",
+//     name: "",
+//     message: "",
+//   },
+//   {
+//     type: "",
+//     name: "",
+//     message: "",
+//   },
+// ];

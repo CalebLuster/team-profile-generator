c5662b6c85ead4b1407a1c30c939337cf93a04c0
Caleb Luster <cwl117@outlook.com>
2022-03-01 16:20:26 -0600

    added pickOne function and wrote it out, testing it soon

--- index.js
+++ index.js
@@ -4,20 +4,48 @@ const path = require("path");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const Choices = require("inquirer/lib/objects/choices");
 const team = [];
 
 const invokeManager = () => {
-return inquirer.prompt([
-    {}
-])
-}
+  return inquirer.prompt([
+    {
+      tpye: "input",
+      name: "name",
+      message: "What is your first name?",
+    },
+  ]);
+};
 const invokeEngineer = () => {
-    return inquirer.prompt([
-        {}
-    ])
-}
+  return inquirer.prompt([
+    {
+      tpye: "input",
+      name: "name",
+      message: "What is your first name?",
+    },
+  ]);
+};
 const invokeIntern = () => {
-    return inquirer.prompt([
-        {}
-    ])
-}
+  return inquirer.prompt([
+    {
+      tpye: "input",
+      name: "name",
+      message: "What is your first name?",
+    },
+  ]);
+};
+const pickOne = () => {
+  return inquirer.prompt([
+    {
+      type: "What you like to do next? (Please choose one)",
+      name: "configTeam",
+      choices: [
+        "Add a new Engineer",
+        "Add a new Intern",
+        "Finish building team",
+      ],
+    },
+  ]);
+};
+
+pickOne();

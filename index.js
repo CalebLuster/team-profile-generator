c87b7b38ad02df64a3f11d8d6c39f039fde4715c
Caleb Luster <cwl117@outlook.com>
2022-03-01 17:05:40 -0600

    added a whole lot of code. Basically added the then(results) fat arrow function to return all the users choices back to them. It then also pushs those answers and creates a new employee and adds it to an empty string which I set at the top

--- index.js
+++ index.js
@@ -8,172 +8,208 @@ const Choices = require("inquirer/lib/objects/choices");
 const team = [];
 
 const invokeManager = () => {
-  return inquirer.prompt([
-    {
-      tpye: "input",
-      name: "name",
-      message: "What is your first name?",
-      validate: (name) => {
-        if (name) {
-          return true;
-        } else {
-          console.log("You must enter a name to continue");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        tpye: "input",
+        name: "name",
+        message: "What is your first name?",
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            console.log("You must enter a name to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "email",
-      message: "What is your email?",
-      validate: (email) => {
-        if (email) {
-          return true;
-        } else {
-          console.log("You must enter a email to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "email",
+        message: "What is your email?",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log("You must enter a email to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "ID",
-      message: "What is your employee ID?",
-      validate: (ID) => {
-        if (ID) {
-          return true;
-        } else {
-          console.log("You must enter a ID number to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "ID",
+        message: "What is your employee ID?",
+        validate: (ID) => {
+          if (ID) {
+            return true;
+          } else {
+            console.log("You must enter a ID number to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "officeNumber",
-      message: "What is your officeNumber?",
-      validate: (officeNumber) => {
-        if (officeNumber) {
-          return true;
-        } else {
-          console.log("You must enter a office number to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "officeNumber",
+        message: "What is your officeNumber?",
+        validate: (officeNumber) => {
+          if (officeNumber) {
+            return true;
+          } else {
+            console.log("You must enter a office number to continue");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+    ])
+    .then((results) => {
+      console.log(results);
+      const manager = new Manager(
+        results.name,
+        results.email,
+        results.ID,
+        results.school
+      );
+      team.push(manager);
+      pickOne();
+    });
 };
 const invokeEngineer = () => {
-  return inquirer.prompt([
-    {
-      tpye: "input",
-      name: "name",
-      message: "What is your first name?",
-      validate: (name) => {
-        if (name) {
-          return true;
-        } else {
-          console.log("You must enter a name to continue");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        tpye: "input",
+        name: "name",
+        message: "What is your first name?",
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            console.log("You must enter a name to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "email",
-      message: "What is your email?",
-      validate: (email) => {
-        if (email) {
-          return true;
-        } else {
-          console.log("You must enter a email to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "email",
+        message: "What is your email?",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log("You must enter a email to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "ID",
-      message: "What is your employee ID?",
-      validate: (ID) => {
-        if (ID) {
-          return true;
-        } else {
-          console.log("You must enter a ID number to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "ID",
+        message: "What is your employee ID?",
+        validate: (ID) => {
+          if (ID) {
+            return true;
+          } else {
+            console.log("You must enter a ID number to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "github",
-      message: "What is your github username?",
-      validate: (github) => {
-        if (github) {
-          return true;
-        } else {
-          console.log("You must enter a GitHub username to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "github",
+        message: "What is your github username?",
+        validate: (github) => {
+          if (github) {
+            return true;
+          } else {
+            console.log("You must enter a GitHub username to continue");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+    ])
+    .then((results) => {
+      console.log(results);
+      const engineer = new Engineer(
+        results.name,
+        results.email,
+        results.ID,
+        results.github
+      );
+      team.push(engineer);
+      pickOne();
+    });
 };
 const invokeIntern = () => {
-  return inquirer.prompt([
-    {
-      tpye: "input",
-      name: "name",
-      message: "What is your first name?",
-      validate: (name) => {
-        if (name) {
-          return true;
-        } else {
-          console.log("You must enter a name to continue");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        tpye: "input",
+        name: "name",
+        message: "What is your first name?",
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            console.log("You must enter a name to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "email",
-      message: "What is your email?",
-      validate: (email) => {
-        if (email) {
-          return true;
-        } else {
-          console.log("You must enter a email to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "email",
+        message: "What is your email?",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log("You must enter a email to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "ID",
-      message: "What is your employee ID?",
-      validate: (ID) => {
-        if (ID) {
-          return true;
-        } else {
-          console.log("You must enter a ID number to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "ID",
+        message: "What is your employee ID?",
+        validate: (ID) => {
+          if (ID) {
+            return true;
+          } else {
+            console.log("You must enter a ID number to continue");
+            return false;
+          }
+        },
       },
-    },
-    {
-      tpye: "input",
-      name: "school",
-      message: "What is the name of your school?",
-      validate: (school) => {
-        if (school) {
-          return true;
-        } else {
-          console.log("You must enter a school to continue");
-          return false;
-        }
+      {
+        tpye: "input",
+        name: "school",
+        message: "What is the name of your school?",
+        validate: (school) => {
+          if (school) {
+            return true;
+          } else {
+            console.log("You must enter a school to continue");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+    ])
+    .then((results) => {
+      console.log(results);
+      const intern = new Intern(
+        results.name,
+        results.email,
+        results.ID,
+        results.school
+      );
+      team.push(intern);
+      pickOne();
+    });
 };
 const pickOne = () => {
   return inquirer.prompt([
